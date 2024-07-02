972e791f06b1ab49b5edfaa8a9418a8c92b982c7
mrkeshav.05 <keshavpundhir69@gmail.com>
2024-07-02 11:20:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import InputBox from './components/InputBox'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-red-400 text-center bg-orange-900 text-4xl'>Currency</h1>
+      <InputBox/>
+    </>
+  )
+}
+
+export default App
